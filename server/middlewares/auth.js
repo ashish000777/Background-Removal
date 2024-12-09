c38cdd64b03421fd20dcd997abe5279e6dab4eb3
ashish000777 <ashishbagate03@gmail.com>
2024-12-09 18:24:35 +0530

    Added token for authorization

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,7 +3,13 @@ import jwt from 'jsonwebtoken'
 // Middleware Function to decode jwt token to get clerkId
 const authUser = async(req, res, next) => {
     try {
-        
+        const {token} = req.headers
+
+        if (!token){
+            return res.json({success: false, message: 'Not Authorized Login Again'})
+        }
+
+        const token_decode = jwt.decode(token)
     } catch (error) {
         console.log(error.message)
         res.json({success:false, message:error.message})
