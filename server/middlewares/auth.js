7fc03da84cb1a4778c814b5f3e02375016ef65b1
ashish000777 <ashishbagate03@gmail.com>
2024-12-09 21:45:58 +0530

    Created context api

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -10,9 +10,11 @@ const authUser = async(req, res, next) => {
         }
 
         const token_decode = jwt.decode(token)
+        req.body.clerkId = token_decode.clerkId
     } catch (error) {
         console.log(error.message)
         res.json({success:false, message:error.message})
+        next()
     }
 }
 
